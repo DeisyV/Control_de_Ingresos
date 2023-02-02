e701042578cf37dd4ef80919256e5f414db09ead
DeisyV <velascodeisy31@gmail.com>
2023-02-01 20:56:44 -0500

    instalacion de plantilla desde cero

---
+++ WebControlGastos/Controllers/GastosController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebControlGastos.Controllers
+{
+    public class GastosController : Controller
+    {
+        // GET: Gastos
+        public ActionResult Gastos()
+        {
+            //return View();
+            return PartialView("~/Views/Gastos/prueba.cshtml");
+
+        }
+    }
+}
