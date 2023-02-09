ea4300a877ba1e5fda18c5ff1ca95bf19e7ac445
DeisyV <velascodeisy31@gmail.com>
2023-02-09 17:50:56 -0500

    creacion de pantallas

---
+++ WebControlGastos/Controllers/RegistrarPersonaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebControlGastos.Controllers
+{
+    public class RegistrarPersonaController : Controller
+    {
+        // GET: RegistrarPersona
+        public ActionResult RegistrarPersona()
+        {
+            return View();
+        }
+    }
+}
